766586bdf20bef55c7126c58a940ca80d031f6f5
yaboiishere <mixaildobrev@gmail.com>
2020-08-10 16:58:25 +0300

    ex50 start

---
+++ excercise506.exs
@@ -0,0 +1,40 @@
+defmodule Math do
+
+  #Implement functions, that solve a quadratic equation using the conditional operators and with.
+  #The coeficients are passed as a tuple, and each conditional operator should
+  # be implemented in a seperate function.
+  def solve_quad({a,b,c}) do
+    cond do
+      a == 0 -> #solve_lineino
+        "x = #{-b}"
+      a != 0 ->
+        case get_disc({a,b,c}) do #get disc
+          {:ok, solution} -> "x1 = x2 = #{solution}"
+          {:quad, {x1, x2}} -> "x1 = #{x1}, x2 = #{x2}"
+          {:error, err} -> err
+        end
+
+    end
+  end
+  def get_disc({a,b,c}) do
+    disc = b*b-4*a*c
+    cond do
+      disc == 0 ->
+        {:ok, -b/2*a}
+      disc > 0 ->
+        {:quad, {(-b + :math.sqrt(disc))/ 2*a, (-b - :math.sqrt(disc))/ 2*a }}
+      disc < 0 ->
+        {:error, "No real solution"}
+    end
+
+  end
+end
+
+defmodule Ex do
+  def ex2(list, char) when is_list(list) do
+    Enum.map(list, fn(el)->
+      "#{el}#{char}"
+    end)
+  end
+
+end
