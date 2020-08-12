7d42c8c09869e46aa6cb1c4db5a94ab598094948
yaboiishere <mixaildobrev@gmail.com>
2020-08-12 08:09:05 +0300

    ex7 done

--- excercise7.ex
+++ excercise7.ex
@@ -1,5 +1,16 @@
 defmodule Rec do
 
-  
+  def con_list(list), do: con_list(list, "")
+  defp con_list([head | tail], acc), do: con_list(tail, acc <> inspect(head))
+  defp con_list([], acc), do: acc
+
+  def sum_list(list), do: sum_list(list, 0)
+  defp sum_list([head | tail], acc), do: sum_list(tail, acc+head)
+  defp sum_list([], acc), do: acc
+
+  def fact(f), do: fact(1,f,1)
+  defp fact(n, f, acc)  when n>f, do: acc
+  defp fact(n, f, acc), do: fact(n+1, f ,acc*n)
+
 
 end
